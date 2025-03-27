bdd9238fc42fee0700b6f1f0320527b4a62eba05
andregiraldo <andrea.girald29@gmail.com>
2025-03-27 11:46:13 -0500

    Update courses.ts
    otra actualiza primera imagen

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -22,7 +22,7 @@ export const courses: Course[] = [
     duration: "60 minutos",
     students: 1,
     price: "$40.000",
-    image: "/lovable-uploads/cartoonmath1.png",
+    image: "/lovable-uploads/cartoonmath2.jpg",
     topics: [
       "Ecuaciones lineales y sistemas",
       "Polinomios y factorización",
