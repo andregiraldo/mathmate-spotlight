78368dc4382cf0381bff28f83c68d54d221866ed
andregiraldo <andrea.girald29@gmail.com>
2025-04-16 13:33:52 -0500

    Update courses.ts

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -42,22 +42,47 @@ export const courses: Course[] = [
     price: "$150.000",
     image: "/lovable-uploads/foto-mate2.png",
     topics: [
-      "Factorización y Polinomios",
-      "Potenciación",
-      "Radicación",
-      "Ecuaciones lineales y sistemas",
-      "Funciones cuadráticas",
-      "Funciones trigonometricas",
-      "Productos notables",
-      "Expresiones racionales",
-      "Exponentes y logaritmos"
+      "Números y Operaciones Básicas",
+      "Ecuaciones de primer grado",
+      "Plano Cartesiano",
+      "Ecuaciones de Segundo Grado",
+      "Factorización",
+      "Producto Notable",
+      "Polinomios",
+      "Sistemas de Ecuaciones",
+      "Exponentes y logaritmos",
+      "Desigualdades",
+      "Funciones Líneales y Cuadráticas",
+      "Radicales",
+      "Simplificación de Fracciones Algebraicas",
+      "Multiplicación y División de Fracciones con Polinomios"
+    ]
+  },
+  {
+    id: "algebra lineal",
+    title: "Álgebra Líneal",
+    description: "Comprende los elementos esenciales del álgebra lineal que sirven como base para la resolución de sistemas y el análisis vectorial",
+    longDescription: "En esta sección introductoria del curso de Álgebra Lineal, explorarás conceptos como vectores, matrices, operaciones básicas y sistemas de ecuaciones lineales. Ideal para estudiantes que están comenzando en el mundo del álgebra lineal o que desean reforzar su comprensión antes de avanzar.",
+    level: "Principiante o Intermedio",
+    duration: "4 semanas",
+    students: 3,
+    price: "$150.000",
+    image: "/lovable-uploads/foto-mate2.png",
+    topics: [
+      "Sistemas de Ecuaciones Líneales - Método Gauss",
+      "Vectores",
+      "Matrices",
+      "Determinantes",
+      "Regla de Cramer",
+      "Vectores en R2 y R3",
+      "Producto Escalar"
     ]
   },
   {
     id: "calculo integral",
     title: "Cálculo Integral",
-    description: "Aprende a analizar datos, hacer predicciones y tomar decisiones basadas en evidencia.",
-    longDescription: "El curso de Estadística y Probabilidad te introduce en el mundo del análisis de datos y la inferencia estadística. Desde la estadística descriptiva hasta los conceptos de probabilidad, distribuciones y pruebas de hipótesis, este curso te dará las herramientas para interpretar datos, hacer predicciones fundamentadas y tomar decisiones basadas en evidencia en diversos campos profesionales.",
+    description: "Aprende a calcular áreas, volúmenes y resolver problemas con funciones continuas usando integrales.",
+    longDescription: "El curso de Cálculo Integral te introduce en el análisis de funciones continuas a través del concepto de la integral. Estudiarás técnicas de integración, aplicaciones geométricas y físicas, y la conexión entre derivadas e integrales mediante el teorema fundamental del cálculo. Este curso te proporcionará una base sólida para resolver problemas en matemáticas, física, economía e ingeniería.",
     level: "Intermedio",
     duration: "4 semanas",
     students: 3,
@@ -65,37 +90,40 @@ export const courses: Course[] = [
     image: "/lovable-uploads/foto-mate3.png",
     topics: [
       "Integrales Indefinidas",
+      "Reglas de Integración",
       "Integrales Definidas",
       "Integración por Partes",
       "Función Primitiva",
-      "Teorema fundamental del calculo"
+      "Ecuaciones Diferenciales",
+      "Teorema fundamental del calculo", 
+      "Aplicación de la Derivada"
     ]
   },
   {
     id: "calculo",
     title: "Cálculo Diferencial",
     description: "Domina las técnicas de diferenciación y sus aplicaciones en problemas del mundo real.",
-    longDescription: "El curso de Cálculo Diferencial explora los conceptos fundamentales del cálculo, centrándose en límites, continuidad y derivadas. Aprenderás a calcular derivadas usando diferentes reglas y técnicas, y cómo aplicarlas para resolver problemas de optimización, tasas relacionadas y análisis de funciones. Este curso es esencial para estudiantes de ciencias, ingeniería y economía.",
+    longDescription: "El curso de Cálculo Diferencial explora los conceptos fundamentales del cálculo, centrándose en límites de una función, continuidad, derivadas, resolver problemas de optimización e identificar máximos y mínimos. Aprenderás a calcular derivadas usando diferentes reglas y técnicas, y cómo aplicarlas para resolver problemas de optimización, tasas relacionadas y análisis de funciones. Este curso es esencial para estudiantes de ciencias, ingeniería y economía.",
     level: "Intermedio",
     duration: "4 semanas",
     students: 3,
     price: "$150.000",
     image: "/lovable-uploads/foto-mate4.png",
     topics: [
-      "Límites y continuidad",
-      "Función",
-      "Puntos Críticos",
-      "Definición de derivada",
-      "Reglas de derivación",
-      "Aplicaciones: máximos y mínimos",
-      "Derivación implícita y tasas relacionadas"
+      "Inecuaciones",
+      "Funciones y Límites",
+      "Derivadas",
+      "Derivadas de Orden Superior",
+      "Máximos y Mínimos",
+      "Derivadas de Funciones Exponenciales y Logarítmicas",
+      "Derivadas de Funciones Trigonométricas"
     ]
   },
   {
     id: "trigonometria",
     title: "Trigonometría",
-    description: "Aprende a analizar datos, hacer predicciones y tomar decisiones basadas en evidencia.",
-    longDescription: "El curso de Estadística y Probabilidad te introduce en el mundo del análisis de datos y la inferencia estadística. Desde la estadística descriptiva hasta los conceptos de probabilidad, distribuciones y pruebas de hipótesis, este curso te dará las herramientas para interpretar datos, hacer predicciones fundamentadas y tomar decisiones basadas en evidencia en diversos campos profesionales.",
+    description: "Explora las funciones trigonométricas y su aplicación en la resolución de triángulos y fenómenos periódicos.",
+    longDescription: "El curso de Trigonometría te permite comprender y aplicar las razones trigonométricas, las funciones seno, coseno y tangente, así como sus gráficas y transformaciones. Aprenderás a resolver triángulos, modelar movimientos periódicos y aplicar conceptos en contextos reales como la física, la arquitectura y la ingeniería. Este curso es esencial para avanzar en matemáticas y ciencias aplicadas.",
     level: "Intermedio",
     duration: "4 semanas",
     students: 3,
@@ -106,9 +134,11 @@ export const courses: Course[] = [
       "Graficas",
       "Razones Trigonometricas",
       "Identidades Trigonometricas",
-      "Estadistica",
       "Estadistica Descriptiva",
-      "Estadistica Diferencial"
+      "Estadistica Diferencial",
+      "Ecuaciones Trigonométricas",
+      "Teorema del seno y el coseno",
+      "Aplicaciones de la Trigonometría"
     ]
   },
   {
@@ -127,7 +157,11 @@ export const courses: Course[] = [
       "Variables aleatorias",
       "Inferencia estadística",
       "Pruebas de hipótesis",
-      "Regresión y correlación"
+      "Regresión y correlación",
+      "Análisis estadistico de datos muestrales",
+      "Tablas de Análisis",
+      "Media, Mediana y Moda",
+      "Varianza y Desviación Estandar"
     ]
   }
 ];
